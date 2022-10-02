a339ce68f8e23c4d9d5cfc46a312600fb4093a34
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-10-02 20:45:38 +0000

    Other assessor  details component implemented

--- components/PublicationsAndReminders.js
+++ components/PublicationsAndReminders.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../styles/PublicationsAndReminders.module.css";
 import InputField from "./InputField";
+import SelectInput from "./SelectInput";
+import TextArea from "./TextArea";
 
 const PublicationsAndReminders = () => {
   return (
@@ -13,17 +15,52 @@ const PublicationsAndReminders = () => {
         <InputField label="Assessor 2" type="date" />
         <InputField label="Assessor 3" type="date" isRequired={false} />
       </div>
-      <span className={styles.title}>Date on which reminders were sent to assessors</span>
+      <span className={styles.title}>
+        Date on which reminders were sent to assessors
+      </span>
       <div className={styles.fields}>
         <InputField label="Assessor 1" type="date" />
         <InputField label="Assessor 2" type="date" />
         <InputField label="Assessor 3" type="date" isRequired={false} />
       </div>
-      <span className={styles.title}>Date on which assessments were received</span>
+      <span className={styles.title}>
+        Date on which assessments were received
+      </span>
       <div className={styles.fields}>
         <InputField label="Assessor 1" type="date" />
         <InputField label="Assessor 2" type="date" />
         <InputField label="Assessor 3" type="date" isRequired={false} />
+      </div>
+      <span className={styles.title}>
+        Nature of response received from assessors
+      </span>
+      <div className={styles.fields}>
+        <InputField label="Assessor 1" type="text" />
+        <InputField label="Assessor 2" type="text" />
+        <InputField label="Assessor 3" type="text" isRequired={false} />
+      </div>
+      <span className={styles.title}>
+        Format of response received from assessors
+      </span>
+      <div className={styles.fields}>
+        <InputField label="Assessor 1" type="text" />
+        <InputField label="Assessor 2" type="text" />
+        <InputField label="Assessor 3" type="text" isRequired={false} />
+      </div>
+      <span className={styles.title}>
+        Date on which acknowledgement letters were sent to assessors
+      </span>
+      <div className={styles.fields}>
+        <InputField label="Assessor 1" type="date" />
+        <InputField label="Assessor 2" type="date" />
+        <InputField label="Assessor 3" type="date" isRequired={false} />
+      </div>
+      <div className={styles.fields}>
+        <TextArea
+          label="Further actions taken"
+          placeholder="Please type here..."
+          isRequired={false}
+        />
       </div>
     </div>
   );
