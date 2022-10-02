494c806999e8004107c03b00b8519e6148a6fe6b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-10-02 12:36:03 +0000

    Added SelectInput to the ApplicationBio component

--- components/ApplicantBio.js
+++ components/ApplicantBio.js
@@ -13,17 +13,28 @@ const ApplicantBio = () => {
     "Physics",
     "Statistics & Act. Science",
   ];
+  const positions = ["Senior Lecturer", "Asscociate Professor", "Professor"];
   return (
     <div className={`${styles["applicant-bio"]}`}>
-      <span className={styles.title}>Bio of Applicant</span>
+      <span className={styles.title}>Basic Information of Applicant</span>
       <div className={styles.fields}>
         <InputField label="First Name" type="text" placeholder="Michael" />
         <InputField label="Last Name" type="text" placeholder="Amponsah" />
         <InputField label="Other Names" type="text" placeholder="Kwame" />
         <SelectInput
           label="Department"
-          placeholder="Please select an option"
+          placeholder="Please select an option. Eg. Computer Science"
           options={departments}
+        />
+        <SelectInput
+          label="Present Status"
+          placeholder="Please select an option. Eg. Snr. Lecturer"
+          options={positions}
+        />
+        <SelectInput
+          label="Position Sought"
+          placeholder="Please select an option. Eg. Snr. Lecturer"
+          options={positions}
         />
       </div>
     </div>
